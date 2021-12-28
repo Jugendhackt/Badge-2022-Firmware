16561a64ff8b34b2ad72439359ecd29a5fdacd87
Jadyn Emma Jäger <jadyn@jadyn.dev>
2021-12-28 16:44:53 +0100

    feat(): Use const() for constants

--- Teebeutel/Device.py
+++ Teebeutel/Device.py
@@ -1,40 +1,41 @@
+from micropython import const
 
-#Pin definitions
-UART_TxD = 1
-UART_RxD = 3
+# Pin definitions)
+UART_TxD = const(1)
+UART_RxD = const(3)
 
-I2C_ID = 1
-I2C_SCL = 25
-I2C_SDA = 26
+I2C_ID = const(1)
+I2C_SCL = const(25)
+I2C_SDA = const(26)
 
-SD_SPI_ID = 2
-SD_SCK = 18
-SD_CS = 5
-SD_MISO = 19
-SD_MOSI = 23
-SD_PATH = '/sd'
+SD_SPI_ID = const(2)
+SD_SCK = const(18)
+SD_CS = const(5)
+SD_MISO = const(19)
+SD_MOSI = const(23)
+SD_PATH = const('/sd')
 
-OLED_SPI_ID = 1
-OLED_CS = 17
-OLED_SCK = 14
-OLED_MOSI = 13
-#OLED_MISO = 12
-OLED_DC = 21
-OLED_RES = 16
+OLED_SPI_ID = const(1)
+OLED_CS = const(17)
+OLED_SCK = const(14)
+OLED_MOSI = const(13)
+# OLED_MISO = const(12)
+OLED_DC = const(21)
+OLED_RES = const(16)
 
-TOUCH_A = None
-TOUCH_B = None
+TOUCH_A = const(None)
+TOUCH_B = const(None)
 
-ADC_BAT = 4
+ADC_BAT = const(4)
 
-IR_Rx = 5
-IR_Tx = 27
+IR_Rx = const(5)
+IR_Tx = const(27)
 
-WS2812_PIN = 32
-WS2812_NUM = 12
+WS2812_PIN = const(32)
+WS2812_NUM = const(12)
 
-BTN_UP = 34
-BTN_DOWN = 35
-BTN_LEFT = 15
-BTN_RIGHT = 22
-BTN_PUSH = 33
+BTN_UP = const(34)
+BTN_DOWN = const(35)
+BTN_LEFT = const(15)
+BTN_RIGHT = const(22)
+BTN_PUSH = const(33)
