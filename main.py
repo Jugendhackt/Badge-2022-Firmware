e33819926fd248bfd937d1fa99289a335103521d
Johanna Dorothea Reichmann <transcaffeine@finally.coffee>
2022-10-01 00:18:42 +0200

    chore: switch to new statusbar rendering

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 from Alpaca.SSD1351 import Display
 
 gc.enable()
-alpaca = Alpaca.Alpaca()
+alpaca = Alpaca.Alpaca('jadyn.dev')
 alpaca.display.init()
 
 class Statusbar:
@@ -73,15 +73,18 @@ class Statusbar:
 colors = [[0, 255, 0], [0, 0, 255], [255, 0, 0], [255, 255, 0], [0, 255, 255], [255, 0, 255], [255, 255, 255]]
 c = 0
 
+statusbar = Statusbar(alpaca, 128, 8)
+
+alpaca.display.fill(alpaca.display.rgb_to_rgb565(0, 0, 0))
+
 while True:
     for i in range(len(colors)):
-        alpaca.display.fill(alpaca.display.rgb_to_rgb565(0, 0, 0))
-        #alpaca.display.fill(alpaca.display.rgb_to_rgb565(colors[i][0], colors[i][1], colors[i][2]))
-        #alpaca.display.text("JugendHackt", 64 - 8 * int(len("JugendHackt")/2), 64 - 4 - 10, 0xfff)
-        #alpaca.display.text("Berlin 2022", 64 - 8 * int(len("Berlin 2022")/2), 64 - 4, 0xfff)
-        charge_stat = alpaca.battery.get_percentage()
-        alpaca.render_text(f"JugendHackt {charge_stat}%", 0, 0xfff)
-        alpaca.render_text("Berlin 2022", 1, 0x000)
+        alpaca.display.fill_rect(0, 8, 128, 128 - 8, 0x000)
+
+        statusbar.render(alpaca.display)
+
+        statusbar.tick(alpaca)
+
         alpaca.render_text(f"UP: {('YES' if alpaca.dpad.up else 'NO')}", 3, 0xfff)
         alpaca.render_text(f"DOWN: {('YES' if alpaca.dpad.down else 'NO')}", 4, 0xfff)
         alpaca.render_text(f"LEFT: {('YES' if alpaca.dpad.left else 'NO')}", 5, 0xfff)
